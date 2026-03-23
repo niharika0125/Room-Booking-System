6a37f50cac2073901253ebb3dcf82ab2edbb91a9
Niharika <kurabalakotaniharika@gmail.com>
2026-03-23 23:50:14 +0530

    UC5: Booking Request

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ class Room {
     int roomNumber;
     boolean isAvailable;
 
-    Room(int roomNumber, boolean isAvailable) {
+    Room(int roomNumber) {
         this.roomNumber = roomNumber;
-        this.isAvailable = isAvailable;
+        this.isAvailable = true;
     }
 }
 
@@ -14,14 +14,19 @@ public class Main {
     public static void main(String[] args) {
         List<Room> rooms = new ArrayList<>();
 
-        rooms.add(new Room(101, true));
-        rooms.add(new Room(102, false));
+        rooms.add(new Room(101));
+        rooms.add(new Room(102));
 
-        System.out.println("Available Rooms:");
+        int requestedRoom = 101;
+
         for (Room r : rooms) {
-            if (r.isAvailable) {
-                System.out.println("Room " + r.roomNumber + " is available");
+            if (r.roomNumber == requestedRoom && r.isAvailable) {
+                r.isAvailable = false;
+                System.out.println("Room " + r.roomNumber + " booked successfully!");
+                return;
             }
         }
+
+        System.out.println("Room not available");
     }
 }
