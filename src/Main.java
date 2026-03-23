20aa8bb69e23da14fe4d5b26de05070dd673bf62
Niharika <kurabalakotaniharika@gmail.com>
2026-03-23 23:41:28 +0530

    UC2: Basic Room Types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+class Room {
+    int roomNumber;
+    String type;
+
+    Room(int roomNumber, String type) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Welcome to Room Booking System!");
+        Room r1 = new Room(101, "Single");
+        Room r2 = new Room(102, "Double");
+
+        System.out.println("Room " + r1.roomNumber + " Type: " + r1.type);
+        System.out.println("Room " + r2.roomNumber + " Type: " + r2.type);
     }
 }
